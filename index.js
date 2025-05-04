e6a0c953cca240bf85fd77325bdf8206b98d91e9
vitorhugogo <hugovictorsilveira@gmail.com>
2025-05-04 02:44:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DiscipuladoForm from "./components/DiscipuladoForm";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <DiscipuladoForm />
+  </React.StrictMode>
+);
